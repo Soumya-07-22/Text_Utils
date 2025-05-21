c008cb41187baa2b45f58774be129b4f32ff6f2e
Soumya Ranjan Mahapatra <soumyamahapatra3212@gmail.com>
2025-05-21 12:56:39 +0530

    Added Analyze2.html and updated Views.py

--- mySite/Views.py
+++ mySite/Views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'Analyze2.html')
 
 def analyze(request):
     djText = request.POST.get('text', 'default')
